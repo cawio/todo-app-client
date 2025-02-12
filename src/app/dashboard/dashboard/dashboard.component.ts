cd45238c5d7c1cfef7ce61b2b361cf7e22204c26
Simon <sberner9@gmail.com>
2025-02-12 20:26:06 +0100

    feat: implement todo list display and fetch functionality in dashboard

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { lastValueFrom } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+export type TodoList = {
+    id: number;
+    userId: number;
+    title: string;
+    createdAt: string;
+    updatedAt: string;
+    tasks: Task[];
+};
+
+export type Task = {
+    id: number;
+    title: string;
+    description: string;
+    dueOn: string;
+    priority: number;
+    completed: boolean;
+    createdAt: string;
+    updatedAt: string;
+    tags: Tag[];
+};
+
+export type Tag = {
+    id: number;
+    name: string;
+};
 
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+    selector: 'app-dashboard',
+    imports: [MatFormFieldModule, ReactiveFormsModule],
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+    readonly #http = inject(HttpClient);
 
+    todoLists: TodoList[] = [];
+
+    async ngOnInit() {
+        this.todoLists = await lastValueFrom(
+            this.#http.get<TodoList[]>(environment.apiUrl + '/todo-lists', {
+                withCredentials: true,
+            })
+        );
+    }
 }
